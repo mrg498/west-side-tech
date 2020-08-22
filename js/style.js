1be6fd3f5d8f3e756a54d35d6f1e530551eeac4b
Miles Grossenbacher <mrg498@nyu.edu>
2020-08-22 16:42:27 -0400

    style index.html header

--- js/style.js
+++ js/style.js
@@ -5,6 +5,11 @@ window.addEventListener('DOMContentLoaded', () => {
     hamburger_icon.addEventListener('click', function(){
         const overlay = document.querySelector('.overlay');
         overlay.classList.toggle('show-overlay');
+        overlay.classList.toggle('hide-overlay');
+        const mobile_nav = document.querySelector('.mobile-nav')
+        mobile_nav.classList.toggle('show-mobile-nav');
+        const bars = document.querySelector('.bars')
+        bars.classList.toggle('transform-bars');
     });
 
 });
